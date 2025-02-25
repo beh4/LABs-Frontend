8ed12e9acbd1c1662665c12e14ead25e6049c8ce
beh4 <rmansyr06@gmail.com>
2025-02-25 20:13:07 +0400

    lab5

---
+++ dist/lab5.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Страница загрузилась");
+    const data = [12, 23, 34, 45];
+    function removeOddNumbers() {
+        let index = 0;
+        const removeNextOdd = () => {
+            if (index < data.length) {
+                if (data[index] % 2 !== 0) {
+                    console.log(`Удалено число: ${data[index]}`);
+                    data.splice(index, 1);
+                    setTimeout(removeNextOdd, 1000);
+                }
+                else {
+                    index++;
+                    setTimeout(removeNextOdd, 1000);
+                }
+            }
+        };
+        removeNextOdd();
+    }
+    removeOddNumbers();
+    function createLogger(message) {
+        const Message = `Сообщение: ${message}`;
+        return () => {
+            console.log(Message);
+        };
+    }
+    const greet = createLogger("Замыкание");
+    greet();
+});
